ddf7cca3f392d7299dcf4aa64ca42326b0eb1aa2
Hugo Araya Carrasco <hugo.araya.carrasco@gmail.com>
2026-05-25 09:39:25 -0400

    Primer programa

---
+++ primer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    // Declaraciones
+    int a, b;
+    printf("Ingrese un numero entero: ");
+    scanf("%d", &a);
+    b = 2 * a;
+    printf("Resultado %d\n", b);
+    return 0;
+}
